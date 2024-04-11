79dd2c7fd3ee380fc0672d9049501e76a2dd598a
JEONG-J <ikejhc159@gmail.com>
2024-04-12 00:56:46 +0900

    Feat: 컬러 정의
    사용에 필요한 컬러 정의

---
+++ BookSpud/BookSpudApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSpudApp.swift
+//  BookSpud
+//
+//  Created by 정의찬 on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BookSpudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookSpud/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookSpud
+//
+//  Created by 정의찬 on 4/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.gray04)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
