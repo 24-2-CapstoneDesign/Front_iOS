b2bdf0720456034d9edfe58900a5fb7bf3eb7b73
JEONG-J <ikejhc159@gmail.com>
2024-04-28 21:53:02 +0900

    Feat: 프로필 사진 지정 기능 구현
    프로필 사진 지정 시 앨범에서 사진 불러와서 저장되도록 처리

---
+++ BookSpud/GlobalGeneric/ImagePicker/ImageHandling.swift
@@ -0,0 +1,17 @@
+//
+//  ImageHandling.swift
+//  BookSpud
+//
+//  Created by 정의찬 on 4/28/24.
+//
+
+import Foundation
+import SwiftUI
+
+protocol ImageHandling: AnyObject {
+    func addImage(_ images: UIImage?) // 이미지 추가 함수
+    func showImagePicker() // 이미지 피커 불러오기
+    
+    var isImagePickerPresendted: Bool { get set } // 이미지 피커 선택되었는지 확인
+    var selectedImage: UIImage? { get }
+}
