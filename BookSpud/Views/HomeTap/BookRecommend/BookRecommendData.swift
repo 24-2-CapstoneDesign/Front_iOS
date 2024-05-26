656c7ae76ddea1c3218e9b8b8b812b858f327473
JEONG-J <ikejhc159@gmail.com>
2024-05-26 23:46:00 +0900

    Feat: 추천책 디테일 뷰 유저 데이터 파일 분리
    추천 받은책 디테일 뷰 유저 데이터 항목 분리하여 따로 관리

--- BookSpud/Views/HomeTap/BookRecommend/BookRecommendData.swift
+++ BookSpud/Views/HomeTap/BookRecommend/BookRecommendData.swift
@@ -20,18 +20,10 @@ struct BookRecommendDetailData: Codable, Hashable{
     var introduce: String
 }
 
-// TODO: - 데이터 매핑 하기 API, id 값 없는 상태로 하기
-
-// MARK: - EmotionBookMarkData
-
-struct EmotionBookMarkData: Codable, Hashable {
-    var bookMarkProfile: String
-    var bookMarkName: String
-}
-
 
 final class sampleDataLists {
     static let datalist = BookRecommendData(information: [
         BookRecommendDetailData(bookCoverUrl: "https://contents.kyobobook.co.kr/sih/fit-in/458x0/pdt/9791168418011.jpg", bookName: "Book One", author: "Author A", subject: "소설", price: 12800, introduce: "하하하하하 재밌어요!!")
     ])
+    
 }
---
+++ BookSpud/Views/HomeTap/BookRecommend/BookRecommendDetail/EmotionUserProfile/EmotionUserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  EmotionUserProfile.swift
+//  BookSpud
+//
+//  Created by 정의찬 on 5/26/24.
+//
+
+import SwiftUI
+
+struct EmotionUserProfile: View {
+    
+    var userProfile: String
+    var userName: String
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
